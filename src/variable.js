d5ff6bf53e7622f4a867f416e25f608233899499
NEO2708 <parthr.2608@gmail.com>
2025-03-08 19:59:04 +0530

    base_url_changed

--- src/variable.js
+++ src/variable.js
@@ -1,6 +1,6 @@
 // const BASE_URL = 'http://localhost:8080/api/v1';
 // const BASE_URL = 'https://coral-app-2i5ef.ondigitalocean.app/api/v1';
-const BASE_URL =  `http://52.207.139.164:8080/api/v1`;
+const BASE_URL =  `https://texly.co.in/api/v1`;
 
 
 export default BASE_URL;
