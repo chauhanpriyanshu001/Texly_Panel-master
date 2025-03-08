add68b62158b33856ce09b1ecf055752d0975fa1
NEO2708 <parthr.2608@gmail.com>
2025-03-08 17:45:06 +0530

    ipadded

--- src/variable.js
+++ src/variable.js
@@ -1,6 +1,6 @@
 // const BASE_URL = 'http://localhost:8080/api/v1';
 // const BASE_URL = 'https://coral-app-2i5ef.ondigitalocean.app/api/v1';
-const BASE_URL =  `https://texly.online/api/v1`;
+const BASE_URL =  `http://52.207.139.164:8080/api/v1`;
 
 
 export default BASE_URL;
