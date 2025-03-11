732da46662adbb6664b9fc0e3d3fbd646cb54fc0
NEO2708 <parthr.2608@gmail.com>
2025-03-11 23:16:19 +0530

    url-chnage-5

--- src/variable.js
+++ src/variable.js
@@ -1,6 +1,6 @@
 // const BASE_URL = 'http://localhost:8080/api/v1';
 // const BASE_URL = 'https://coral-app-2i5ef.ondigitalocean.app/api/v1';
-const BASE_URL =  `https://texly.co.in/api/v1`;
+const BASE_URL =  `https://admin.texly.in/api/v1`;
 
 
 export default BASE_URL;
