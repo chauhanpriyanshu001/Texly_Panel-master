1d4cfa2e0a45bd471314fcba5d549cd274ffc4d7
NEO2708 <parthr.2608@gmail.com>
2025-03-11 19:29:48 +0530

    new-url-3

--- src/variable.js
+++ src/variable.js
@@ -1,6 +1,6 @@
 // const BASE_URL = 'http://localhost:8080/api/v1';
 // const BASE_URL = 'https://coral-app-2i5ef.ondigitalocean.app/api/v1';
-const BASE_URL =  `http://new-backend-env.eba-etbspyrg.ap-south-1.elasticbeanstalk.com/api/v1`;
+const BASE_URL =  `https://texly.co.in/api/v1`;
 
 
 export default BASE_URL;
