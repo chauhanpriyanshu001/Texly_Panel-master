90ab25e996670f0f6f9c61bebaa6b72daa0f19aa
NEO2708 <parthr.2608@gmail.com>
2025-04-03 16:55:22 +0530

    url-change

--- src/Pages/Contactus/ContactUs.js
+++ src/Pages/Contactus/ContactUs.js
@@ -273,7 +273,7 @@ const ChatMessage = ({ message }) => {
     </Box>
   );
 };
-const socket = io("http://localhost:8080");
+const socket = io("http://testserver-env.eba-gbxfvgdm.ap-south-1.elasticbeanstalk.com");
 const ContactUsPage = () => {
   const [contacts, setContacts] = useState([]);
   const [activeContact, setActiveContact] = useState(null);
--- src/variable.js
+++ src/variable.js
@@ -1,6 +1,6 @@
-const BASE_URL = 'http://localhost:8080/api/v1';
+// const BASE_URL = 'http://localhost:8080/api/v1';
 // const BASE_URL = 'https://coral-app-2i5ef.ondigitalocean.app/api/v1';
-// const BASE_URL =  `https://admin.texly.in/api/v1`;
+const BASE_URL =  `http://testserver-env.eba-gbxfvgdm.ap-south-1.elasticbeanstalk.com`;
 
 
 export default BASE_URL;
