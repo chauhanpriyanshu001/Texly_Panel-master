c2c7005c6a472be7cae306f8f7dd87efca73cd00
NEO2708 <parthr.2608@gmail.com>
2025-04-21 11:19:33 +0530

    final

--- src/variable.js
+++ src/variable.js
@@ -1,8 +1,8 @@
-const BASE_URL = 'http://localhost:8080/api/v1';
+// const BASE_URL = 'http://localhost:8080/api/v1';
 // const BASE_URL = 'https://coral-app-2i5ef.ondigitalocean.app/api/v1';
-// const BASE_URL =  `https://admin.texly.in/api/v1`;
+const BASE_URL = `https://admin.texly.in/api/v1`;
 
-const SOS_URL='http://localhost:8080'
-// const SOS_URL='https://admin.texly.in'
+// const SOS_URL='http://localhost:8080'
+const SOS_URL = 'https://admin.texly.in'
 export { SOS_URL };
 export default BASE_URL;
