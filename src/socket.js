97a36d2e1c3e0515aa53f8eb12b9400010955fa8
NEO2708 <parthr.2608@gmail.com>
2025-06-19 14:28:56 +0530

    update

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8080", {
+const socket = io("https://admin.texly.in", {
     reconnection: true,
     reconnectionAttempts: 5,
     reconnectionDelay: 1000,
